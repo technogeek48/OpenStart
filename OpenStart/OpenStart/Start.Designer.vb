9ce4c5c6f282a2248e2b477a55d265d7dbcea6f9
Michael-John <michael.john.tech@gmail.com>
2013-02-23 11:31:54 -0500

    Added basic shapes and essential controls

--- OpenStart/OpenStart/Start.Designer.vb
+++ OpenStart/OpenStart/Start.Designer.vb
@@ -24,6 +24,10 @@ Partial Class Start
     Private Sub InitializeComponent()
         Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
         Me.RectangleShape1 = New Microsoft.VisualBasic.PowerPacks.RectangleShape()
+        Me.UserPictureBorder = New Microsoft.VisualBasic.PowerPacks.RectangleShape()
+        Me.RectangleShape3 = New Microsoft.VisualBasic.PowerPacks.RectangleShape()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ShapeContainer1
@@ -31,29 +35,62 @@ Partial Class Start
         Me.ShapeContainer1.Location = New System.Drawing.Point(0, 0)
         Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
         Me.ShapeContainer1.Name = "ShapeContainer1"
-        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.RectangleShape1})
+        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.RectangleShape3, Me.UserPictureBorder, Me.RectangleShape1})
         Me.ShapeContainer1.Size = New System.Drawing.Size(428, 560)
         Me.ShapeContainer1.TabIndex = 0
         Me.ShapeContainer1.TabStop = False
         '
         'RectangleShape1
         '
+        Me.RectangleShape1.BorderColor = System.Drawing.Color.Transparent
+        Me.RectangleShape1.FillColor = System.Drawing.Color.Lime
+        Me.RectangleShape1.FillGradientColor = System.Drawing.Color.Lime
+        Me.RectangleShape1.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid
         Me.RectangleShape1.Location = New System.Drawing.Point(0, 0)
         Me.RectangleShape1.Name = "RectangleShape1"
-        Me.RectangleShape1.Size = New System.Drawing.Size(427, 24)
+        Me.RectangleShape1.SelectionColor = System.Drawing.Color.Transparent
+        Me.RectangleShape1.Size = New System.Drawing.Size(428, 24)
         '
-        'Form1
+        'UserPictureBorder
+        '
+        Me.UserPictureBorder.BorderColor = System.Drawing.SystemColors.ActiveCaption
+        Me.UserPictureBorder.BorderWidth = 10
+        Me.UserPictureBorder.Location = New System.Drawing.Point(302, 4)
+        Me.UserPictureBorder.Name = "UserPictureBorder"
+        Me.UserPictureBorder.Size = New System.Drawing.Size(69, 68)
+        '
+        'RectangleShape3
+        '
+        Me.RectangleShape3.Location = New System.Drawing.Point(9, 33)
+        Me.RectangleShape3.Name = "RectangleShape3"
+        Me.RectangleShape3.Size = New System.Drawing.Size(277, 517)
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(301, 3)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(71, 70)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Start
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(428, 560)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.ShapeContainer1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
-        Me.Name = "Form1"
+        Me.Name = "Start"
+        Me.TransparencyKey = System.Drawing.Color.Lime
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents ShapeContainer1 As Microsoft.VisualBasic.PowerPacks.ShapeContainer
     Friend WithEvents RectangleShape1 As Microsoft.VisualBasic.PowerPacks.RectangleShape
+    Friend WithEvents UserPictureBorder As Microsoft.VisualBasic.PowerPacks.RectangleShape
+    Friend WithEvents RectangleShape3 As Microsoft.VisualBasic.PowerPacks.RectangleShape
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
 
 End Class
