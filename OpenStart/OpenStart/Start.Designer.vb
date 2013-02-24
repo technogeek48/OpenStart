ff146742fd6100fe2ec0466d3bd9e45557f873dd
Michael-John <michael.john.tech@gmail.com>
2013-02-24 15:53:27 -0500

    LOTS of UI additions
    (starting to look like a start menu)

--- OpenStart/OpenStart/Start.Designer.vb
+++ OpenStart/OpenStart/Start.Designer.vb
@@ -23,10 +23,14 @@ Partial Class Start
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
-        Me.RectangleShape1 = New Microsoft.VisualBasic.PowerPacks.RectangleShape()
-        Me.UserPictureBorder = New Microsoft.VisualBasic.PowerPacks.RectangleShape()
+        Me.RectangleShape2 = New Microsoft.VisualBasic.PowerPacks.RectangleShape()
+        Me.LineShape1 = New Microsoft.VisualBasic.PowerPacks.LineShape()
         Me.RectangleShape3 = New Microsoft.VisualBasic.PowerPacks.RectangleShape()
+        Me.UserPictureBorder = New Microsoft.VisualBasic.PowerPacks.RectangleShape()
+        Me.RectangleShape1 = New Microsoft.VisualBasic.PowerPacks.RectangleShape()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -35,10 +39,45 @@ Partial Class Start
         Me.ShapeContainer1.Location = New System.Drawing.Point(0, 0)
         Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
         Me.ShapeContainer1.Name = "ShapeContainer1"
-        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.RectangleShape3, Me.UserPictureBorder, Me.RectangleShape1})
+        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.RectangleShape2, Me.LineShape1, Me.RectangleShape3, Me.UserPictureBorder, Me.RectangleShape1})
         Me.ShapeContainer1.Size = New System.Drawing.Size(428, 560)
         Me.ShapeContainer1.TabIndex = 0
         Me.ShapeContainer1.TabStop = False
+        '
+        'RectangleShape2
+        '
+        Me.RectangleShape2.BackColor = System.Drawing.Color.Silver
+        Me.RectangleShape2.BorderColor = System.Drawing.Color.Transparent
+        Me.RectangleShape2.FillColor = System.Drawing.Color.WhiteSmoke
+        Me.RectangleShape2.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid
+        Me.RectangleShape2.Location = New System.Drawing.Point(10, 511)
+        Me.RectangleShape2.Name = "RectangleShape2"
+        Me.RectangleShape2.SelectionColor = System.Drawing.Color.Transparent
+        Me.RectangleShape2.Size = New System.Drawing.Size(276, 39)
+        '
+        'LineShape1
+        '
+        Me.LineShape1.Name = "LineShape1"
+        Me.LineShape1.X1 = 10
+        Me.LineShape1.X2 = 286
+        Me.LineShape1.Y1 = 510
+        Me.LineShape1.Y2 = 510
+        '
+        'RectangleShape3
+        '
+        Me.RectangleShape3.BackColor = System.Drawing.Color.White
+        Me.RectangleShape3.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque
+        Me.RectangleShape3.Location = New System.Drawing.Point(9, 33)
+        Me.RectangleShape3.Name = "RectangleShape3"
+        Me.RectangleShape3.Size = New System.Drawing.Size(277, 517)
+        '
+        'UserPictureBorder
+        '
+        Me.UserPictureBorder.BorderColor = System.Drawing.SystemColors.ActiveCaption
+        Me.UserPictureBorder.BorderWidth = 10
+        Me.UserPictureBorder.Location = New System.Drawing.Point(302, 4)
+        Me.UserPictureBorder.Name = "UserPictureBorder"
+        Me.UserPictureBorder.Size = New System.Drawing.Size(69, 68)
         '
         'RectangleShape1
         '
@@ -51,40 +90,56 @@ Partial Class Start
         Me.RectangleShape1.SelectionColor = System.Drawing.Color.Transparent
         Me.RectangleShape1.Size = New System.Drawing.Size(428, 24)
         '
-        'UserPictureBorder
-        '
-        Me.UserPictureBorder.BorderColor = System.Drawing.SystemColors.ActiveCaption
-        Me.UserPictureBorder.BorderWidth = 10
-        Me.UserPictureBorder.Location = New System.Drawing.Point(302, 4)
-        Me.UserPictureBorder.Name = "UserPictureBorder"
-        Me.UserPictureBorder.Size = New System.Drawing.Size(69, 68)
-        '
-        'RectangleShape3
-        '
-        Me.RectangleShape3.Location = New System.Drawing.Point(9, 33)
-        Me.RectangleShape3.Name = "RectangleShape3"
-        Me.RectangleShape3.Size = New System.Drawing.Size(277, 517)
-        '
         'PictureBox1
         '
+        Me.PictureBox1.BackColor = System.Drawing.Color.White
         Me.PictureBox1.Location = New System.Drawing.Point(301, 3)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(71, 70)
         Me.PictureBox1.TabIndex = 1
         Me.PictureBox1.TabStop = False
         '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(21, 520)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(252, 20)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "Search not implemented"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.White
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightBlue
+        Me.Button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightBlue
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(12, 478)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(272, 28)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = ">    All Programs"
+        Me.Button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button1.UseVisualStyleBackColor = False
+        '
         'Start
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.RoyalBlue
         Me.ClientSize = New System.Drawing.Size(428, 560)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.ShapeContainer1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Location = New System.Drawing.Point(50, 50)
         Me.Name = "Start"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
         Me.TransparencyKey = System.Drawing.Color.Lime
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents ShapeContainer1 As Microsoft.VisualBasic.PowerPacks.ShapeContainer
@@ -92,5 +147,9 @@ Partial Class Start
     Friend WithEvents UserPictureBorder As Microsoft.VisualBasic.PowerPacks.RectangleShape
     Friend WithEvents RectangleShape3 As Microsoft.VisualBasic.PowerPacks.RectangleShape
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents RectangleShape2 As Microsoft.VisualBasic.PowerPacks.RectangleShape
+    Friend WithEvents LineShape1 As Microsoft.VisualBasic.PowerPacks.LineShape
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 
 End Class
--- OpenStart/OpenStart/Start.vb
+++ OpenStart/OpenStart/Start.vb
@@ -1,3 +1,14 @@
 ﻿Public Class Start
 
+    Private Sub Start_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'getUserPicture
+        'getUserColor{
+        '   store in my.settings
+        '   me.BackroundColor = my.settings.usercolor
+    End Sub
+
+    Private Sub Start_LostFocus(sender As Object, e As EventArgs) Handles Me.LostFocus
+        My.Settings.Save()
+        Me.Hide()
+    End Sub
 End Class
